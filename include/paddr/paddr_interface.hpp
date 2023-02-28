5b28fab655ee5c22e6c0380d37ef1a5bf126c7cc
hgh <1713795460@qq.com>
2023-02-28 23:15:10 +0800

    add uart8258 in device and paddr

--- include/paddr/paddr_interface.hpp
+++ include/paddr/paddr_interface.hpp
@@ -1,25 +1,23 @@
 #ifndef __PADDR_IF_HH__
 #define __PADDR_IF_HH__
 
-#include <cstdint>
-#include <cassert>
-#include <cstdio>
 #include <memory>
 #include <vector>
 #include <queue>
 #include "common.hpp"
-#include "macro.hpp"
 #include "testbench/difftest/struct.hpp"
 #include "easylogging++.h"
+#include <mutex>
 
-class bit_mask {
+class bit_mask {/*{{{*/
     public:
         int len;
         word_t mask;
         bit_mask(int _len):
             len(_len),
             mask(BITMASK(_len)) {}
-};
+};/*}}}*/
+
 class AddrIntv {// address interval{{{
     public:
     // example: [0x100,0x1ff]
@@ -83,40 +81,68 @@ class Pmem : public PaddrInterface  {/*{{{*/
 };/*}}}*/
 
 class PaddrConfreg: public PaddrInterface {/*{{{*/
+    enum {
+        CONFREG_POSTIVE, // when uart has output read negtive queue and check
+        CONFREG_NEGTIVE,
+    };
     // physical address = [0x1faf0000,0x1fafffff]
     private:
-        uint32_t cr[8];
-        uint32_t switch_data;
-        uint32_t switch_inter_data;
-        uint32_t timer;
-        uint32_t led;
-        uint32_t led_rg0;
-        uint32_t led_rg1;
-        uint32_t num;
-        uint32_t simu_flag;
-        uint32_t io_simu;
-        uint8_t virtual_uart;
-        uint32_t open_trace;
-        uint32_t num_monitor;
-        int diff_mode; // not a physical register, using as uart difftest
-        std::queue<uint8_t>* diff_queue; // not a physical register, using as uart difftest
+    uint32_t cr[8];
+    uint32_t switch_data;
+    uint32_t switch_inter_data;
+    uint32_t timer;
+    uint32_t led;
+    uint32_t led_rg0;
+    uint32_t led_rg1;
+    uint32_t num;
+    uint32_t simu_flag;
+    uint32_t io_simu;
+    uint8_t virtual_uart;
+    uint32_t open_trace;
+    uint32_t num_monitor;
+    int diff_mode; // not a physical register, using as uart difftest
+    std::queue<uint8_t>* diff_queue; // not a physical register, using as uart difftest
     public:
-        std::queue <uint8_t> uart_queue;
-        uint32_t confreg_read = 0;
-        uint32_t confreg_write = 0;
-        PaddrConfreg(bool simulation = false,
-                el::Logger* input_logger = el::Loggers::getLogger("default"));
-        void tick();
-        bool do_read (word_t addr, wen_t info, word_t* data);
-        bool do_write(word_t addr, wen_t info, const word_t data);
-        void set_switch(uint8_t value);
-        uint32_t get_num();
-        void set_difftest_mode(int mode, std::queue<uint8_t>* negtive_queue);
-        PaddrInterface* deep_copy();
+    std::queue <uint8_t> uart_queue;
+    uint32_t confreg_read = 0;
+    uint32_t confreg_write = 0;
+    PaddrConfreg(bool simulation = false,
+            el::Logger* input_logger = el::Loggers::getLogger("default"));
+    void tick();
+    bool do_read (word_t addr, wen_t info, word_t* data);
+    bool do_write(word_t addr, wen_t info, const word_t data);
+    void set_switch(uint8_t value);
+    uint32_t get_num();
+    void set_difftest_mode(int mode, std::queue<uint8_t>* negtive_queue);
+    PaddrInterface* deep_copy();
 };/*}}}*/
 
-enum {
-    CONFREG_POSTIVE, // when uart has output read negtive queue and check
-    CONFREG_NEGTIVE,
-};
+class Puart8250: public PaddrInterface {/*{{{*/
+    public:
+        Puart8250();
+        bool do_read (word_t addr, wen_t info, word_t* data);
+        bool do_write(word_t addr, wen_t info, const word_t data);
+        void putc(char c);
+        char getc();
+        bool irq();
+        bool exist_tx();
+    private:
+        bool DLAB();
+        void update_IIR();
+        const static uint64_t UART_RX = 0;
+        const static uint64_t UART_TX = 0;
+        std::queue <char> rx;
+        std::queue <char> tx;
+        std::mutex rx_lock;
+        std::mutex tx_lock;
+
+        bool thr_empty;
+        // regs
+        unsigned char DLL;
+        unsigned char DLM;
+        unsigned char IER;
+        unsigned char LCR;
+        unsigned char IIR;
+        unsigned char MCR;
+};/*}}}*/
 #endif
---
+++ src/device/Puart8250.cpp
@@ -0,0 +1,175 @@
+#include "paddr/paddr_interface.hpp"
+
+#define UART8250_TX_RX_DLL  0
+#define UART8250_IER_DLM    1
+#define UART8250_IIR_FCR    2
+#define UART8250_LCR        3
+#define UART8250_MCR        4
+#define UART8250_LSR        5
+#define UART8250_MSR        6
+#define UART8250_SCR        7
+
+#define UART8250_IER_RDA    1   // Received data available
+#define UART8250_IER_THRE   2   // Transmitter holding register empty
+#define UART8250_IER_LSRC   4   // Receiver line status register chagne
+#define UART8250_IER_MSRC   8   // Modem status register change
+
+Puart8250::Puart8250():
+    thr_empty(false), 
+    DLL(0), DLM(0), IER(0), 
+    LCR(0b00000011), IIR (0), MCR (0) {}
+
+bool Puart8250::do_read (word_t addr, wen_t info, word_t* data){/*{{{*/
+    std::unique_lock<std::mutex> lock(rx_lock);
+    if (info.size != 1) log_pt->error("read uart8250 size not 1");
+    switch (addr) {
+        case UART8250_TX_RX_DLL: {
+                                     if (DLAB()) {
+                                         // DLL
+                                         *data = DLL;
+                                     }
+                                     else {
+                                         // RX
+                                         if (!rx.empty()) {
+                                             *data = rx.front();
+                                             rx.pop();
+                                         }
+                                     }
+                                     break;
+                                 }
+        case UART8250_IER_DLM: {
+                                   if (DLAB()) {
+                                       // DLM
+                                       *data = DLM;
+                                   }
+                                   else {
+                                       // IER
+                                       *data = IER;
+                                   }
+                                   break;
+                               }
+        case UART8250_IIR_FCR: {
+                                   // IIR
+                                   update_IIR();
+                                   *data = IIR;
+                                   thr_empty = false;
+                                   break;
+                               }
+        case UART8250_LCR: {
+                               *data = LCR;
+                               break;
+                           }
+        case UART8250_MCR: {
+                               *data = MCR;
+                               break;
+                           }
+        case UART8250_LSR: {
+                               *data = (!rx.empty()) | (1<<5) | (1<<6);
+                               break;
+                           }
+        case UART8250_MSR: {
+                               // Carrier detect = 1
+                               // Clear to send = 1
+                               *data = (1<<7) | (1<<4);
+                               break;
+                           }
+        case UART8250_SCR: {
+                               *data = 0;
+                               break;
+                           }
+        default:
+                           assert(false);
+    }
+    return true;
+}/*}}}*/
+bool Puart8250::do_write(word_t addr, wen_t info, const word_t data){/*{{{*/
+    std::unique_lock<std::mutex> lock_tx(tx_lock);
+    std::unique_lock<std::mutex> lock_rx(rx_lock);
+    bool res = true;
+    if (info.size != 1) log_pt->error("write uart8250 size not 1");
+    switch (addr) {
+        case UART8250_TX_RX_DLL: {
+                                     if (DLAB()) {
+                                         DLL = data;
+                                     }
+                                     else {
+                                         tx.push(static_cast<char>(data));
+                                         thr_empty = false;
+                                     }
+                                     break;
+                                 }
+        case UART8250_IER_DLM: {
+                                   if (DLAB()) {
+                                       DLM = data;
+                                   }
+                                   else {
+                                       IER = data;
+                                   }
+                                   break;
+                               }
+        case UART8250_IIR_FCR: {
+                                   // FCR allow clear FIFO
+                                   if ( (data) & (1 << 1)) {
+                                       while (!rx.empty()) rx.pop();
+                                   }
+                                   if ( (data) & (1 << 2)) {
+                                       while (!tx.empty()) tx.pop();
+                                   }
+                                   break;
+                               }
+        case UART8250_LCR: {
+                               LCR = data;
+                               break;
+                           }
+        case UART8250_MCR: {
+                               MCR = data;
+                               break;
+                           }
+        case UART8250_LSR:
+        case UART8250_MSR: 
+        case UART8250_SCR: 
+        default:
+                           log_pt->error("write uart8250 addr %v error", addr);
+                           res = false;
+    }
+    return res;
+}/*}}}*/
+
+void Puart8250::putc(char c) {
+    std::unique_lock<std::mutex> lock(rx_lock);
+    rx.push(c);
+}
+char Puart8250::getc() {
+    std::unique_lock<std::mutex> lock(tx_lock);
+    if (!tx.empty()) {
+        char res = tx.front();
+        tx.pop();
+        if (tx.empty()) thr_empty = true;
+        return res;
+    }
+    else return EOF;
+}
+bool Puart8250::irq() {
+    update_IIR();
+    return !(IIR & 1);
+}
+bool Puart8250::exist_tx() {
+    std::unique_lock<std::mutex> lock(tx_lock);
+    return !tx.empty();
+}
+bool Puart8250::DLAB() {
+    return (LCR >> 7) != 0;
+}
+void Puart8250::update_IIR() {
+    IIR = 3 << 6; // FIFO Enabled
+    bool no_int = true;
+    if ( (IER & UART8250_IER_THRE) && thr_empty) {
+        IIR |= (1<<1);
+        no_int = false;
+    }
+    if ( (IER & UART8250_IER_RDA) && !rx.empty()) {
+        IIR |= (2<<1);
+        no_int = false;
+    }
+    IIR |= no_int;
+}
