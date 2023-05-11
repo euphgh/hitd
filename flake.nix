9d358659eefcec4a2740c0111c94c119457b9ee8
Guanghui Hu <120L052208@hit.stu.edu.cn>
2023-05-11 21:54:50 +0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A shell for HITD";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            verilator
+            readline
+            ncurses
+            bison
+            flex
+            fmt
+            fd
+          ];
+          nativeBuildInputs = with pkgs; [
+            llvmPackages_15.libllvm
+          ];
+          # you can write ```export VSRC_HOME="/you/vcode/path"```
+          # to shellHook for exporting manually every times
+          shellHook = ''
+            export HITD_HOME=$(pwd)
+          '';
+        };
+      });
+}
