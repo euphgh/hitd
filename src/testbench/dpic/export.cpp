8bdc4f8e2d231143ad3bb03020ab02eda9cc4e5f
Guanghui Hu <120L052208@hit.stu.edu.cn>
2023-05-11 11:29:10 +0800

    fix master export.cpp

--- src/testbench/dpic/export.cpp
+++ src/testbench/dpic/export.cpp
@@ -1,83 +1,35 @@
-#include "testbench/dpic.hpp"
 #include "Vmycpu_top__Dpi.h"
-#include "debug.hpp"
 #include "cp0.hpp"
+#include "debug.hpp"
+#include "testbench/dpic.hpp"
 
-static svScope RegFile_u ; 
-static svScope u_PrimaryBranchAmend ; 
-static svScope u_WriteBack ; 
-static svScope u_PrimaryExceptionProcessor ; 
+/* initalize dpi */
+void dpi_init() { TODO(); }
 
-void dpi_init(){
-    RegFile_u = svGetScopeFromName("TOP.mycpu_top.u_Main.u_ID.RegFile_u"); 
-    u_PrimaryBranchAmend = svGetScopeFromName("TOP.mycpu_top.u_Main.u_PrimaryBranchAmend"); 
-    u_WriteBack = svGetScopeFromName("TOP.mycpu_top.u_Main.u_WriteBack"); 
-    u_PrimaryExceptionProcessor = svGetScopeFromName("TOP.mycpu_top.u_Main.u_PrimaryExceptionProcessor"); 
-}
+/* pass regfile num and get the value of arch regfile in this cycle */
+uint32_t dpi_regfile(uint8_t num) { TODO(); }
 
-uint32_t dpi_regfile(uint8_t num){
-    svSetScope(RegFile_u);
-    return get_regfile(num);
-}
+/* return the number of retire instructions in this cycle */
+uint8_t dpi_retire() { TODO(); }
 
-uint8_t dpi_retire(){
-    svSetScope(u_PrimaryBranchAmend);
-    bool down = commit0();
+/* return the PC value of the last retire instruction in this cycle */
+uint32_t dpi_retirePC() { TODO(); }
 
-    svSetScope(u_WriteBack);
-    bool up = commit1();
-    return up + down;
-}
+/* return the two regiter value: cat(hi,lo) */
+uint64_t dpi_get_hilo() { TODO(); }
 
-uint32_t dpi_retirePC(){
-    svSetScope(u_WriteBack);
-    uint32_t last_pc = commitPC1();
-    if (!commit1()){
-        svSetScope(u_PrimaryBranchAmend);
-        last_pc = commitPC0();
-    }
-    return last_pc;
-}
+/* return 0 if all retire instruction is not interrrupted
+ * else return which instruction is interrrupted */
+uint8_t dpi_interrupt_seq() { TODO(); }
 
-void dpi_get_debug_info0(debug_info_t & debug_info){
-    svSetScope(u_PrimaryBranchAmend);
-    debug_info.pc = commitPC0();
-    debug_info.wen = commitWEN0();
-    debug_info.wnum = commitWNUM0();
-    debug_info.wdata = commitWDATA0();
-}
+/* get CP0 value by rd and select */
+uint32_t dpi_get_cp0(int rd, int sel) { TODO(); }
 
-void dpi_get_debug_info1(debug_info_t & debug_info){
-    svSetScope(u_WriteBack);
-    debug_info.pc = commitPC1();
-    debug_info.wen = commitWEN1();
-    debug_info.wnum = commitWNUM1();
-    debug_info.wdata = commitWDATA1();
-}
+/* return true if cp0 change in this cycle
+ * and write the PC of instruction who
+ * change the cp0 */
+bool dpi_is_cp0_change(uint32_t *changed_pc) { TODO(); }
 
-uint64_t dpi_get_hilo(){
-    TODO();
-}
-
-uint8_t dpi_interrupt_seq(){
-    svSetScope(u_WriteBack);
-    uint8_t res = 0;
-    if (is_interrupt()){
-        svSetScope(u_PrimaryBranchAmend);
-        res = commit0() ? 2 : 1;
-    }
-    return res;
-}
-uint32_t dpi_get_cp0(int rd, int sel){
-    svSetScope(u_PrimaryExceptionProcessor);
-    return get_cp0_value(rd<<3|sel);
-}
-
-bool dpi_is_cp0_change(uint32_t* changed_pc){
-    svSetScope(u_PrimaryExceptionProcessor);
-    bool changed = cp0_change();
-    if (changed) {
-        *changed_pc = cp0_change_pc();
-    }
-    return changed;
-}
+/* hitd user do not pay attention, only contributor need */
+void dpi_get_debug_info0(debug_info_t &debug_info) { TODO(); }
+void dpi_get_debug_info1(debug_info_t &debug_info) { TODO(); }
