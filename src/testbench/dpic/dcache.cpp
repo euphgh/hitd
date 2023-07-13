8c5dbec3e8cc8dffe3190fc6e69b7b8c7b4b4a6b
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-07-13 22:55:01 +0800

    [dcache]: add cache instr log

--- src/testbench/dpic/dcache.cpp
+++ src/testbench/dpic/dcache.cpp
@@ -1,8 +1,8 @@
 #include "common.hpp"
+#include "macro.hpp"
 #include "svdpi.h"
 #include "testbench/difftest/global_info.hpp"
 #include "testbench/sim_state.hpp"
-#include <cstdint>
 #include <fmt/core.h>
 #include <iomanip>
 #include <map>
@@ -12,6 +12,16 @@
 #define dclog(str, ...) mycpu_log->trace(fmt::format("[C] " str, ##__VA_ARGS__))
 enum CacheMainState { run, miss, readDram, refill, uncache, instr };
 enum CacheWriteState { wIdel, wReq, wData, waitwBack };
+enum CacheOp {
+  IndexInvalidI = 0b00000,
+  IndexWriteBackInvalidD = 0b00001,
+  IndexStoreTagI = 0b01000,
+  IndexStoreTagD = 0b01001,
+  HitInvalidI = 0b10000,
+  HitInvalidD = 0b10001,
+  HitWriteBackInvalidD = 0b10101,
+};
+
 static std::map<CacheMainState, std::string> mainStateStr = {
     std::make_pair(run, "run"),           std::make_pair(miss, "miss"),
     std::make_pair(readDram, "readDram"), std::make_pair(refill, "refill"),
@@ -23,6 +33,15 @@ static std::map<CacheWriteState, std::string> writeStateStr = {
     std::make_pair(wData, "wData"),
     std::make_pair(waitwBack, "waitBack"),
 };
+static std::map<CacheOp, std::string> cacheOpStr = {
+    std::make_pair(IndexInvalidI, "IC Idx Invalid"),
+    std::make_pair(IndexWriteBackInvalidD, "DC Idx WB Invalid"),
+    std::make_pair(IndexStoreTagI, "IC Idx Store Tag"),
+    std::make_pair(IndexStoreTagD, "DC Idx Store Tag"),
+    std::make_pair(HitInvalidI, "IC Hit Invalid"),
+    std::make_pair(HitInvalidD, "DC Hit Invalid"),
+    std::make_pair(HitWriteBackInvalidD, "DC Hit WB Invalid"),
+};
 struct CacheState {
   CacheMainState mainState;
   CacheWriteState writeState;
@@ -32,6 +51,8 @@ struct CacheState {
   bool isWrite;
   word_t writeData;
   bool cancel;
+  CacheOp instrOp;
+  bool instrOk;
 };
 
 static CacheState state[2] = {{run, wIdel, 0}, {run, wIdel, 0}};
@@ -55,14 +76,14 @@ bool stateIsSame() {
   return res;
 }
 
-extern "C" void v_difftest_CacheRun(char io_mainState, svBit io_hasValid,
-                                    int io_reqAddr, svBit io_isUncache,
-                                    svBit io_isWrite, int io_writeData,
-                                    svBit io_cancel, int io_retData,
-                                    char io_hitWays, svBit io_isHit,
-                                    char io_victimWay, int io_vicTag,
-                                    svBit io_vicDirty, svBit io_vicValid,
-                                    const int *io_vicLine, char io_writeState) {
+extern "C" void
+v_difftest_CacheRun(char io_mainState, svBit io_hasValid, int io_reqAddr,
+                    svBit io_isUncache, svBit io_isWrite, int io_writeData,
+                    svBit io_cancel, int io_retData, char io_hitWays,
+                    svBit io_isHit, char io_victimWay, int io_vicTag,
+                    svBit io_vicDirty, svBit io_vicValid, const int *io_vicLine,
+                    char io_writeState, char io_instrOp, svBit io_instrOk,
+                    svBit io_instrRetire) {
   std::swap(oldMap, newMap);
   state[newMap].mainState = (CacheMainState)io_mainState;
   state[newMap].writeState = (CacheWriteState)io_writeState;
@@ -71,6 +92,8 @@ extern "C" void v_difftest_CacheRun(char io_mainState, svBit io_hasValid,
   state[newMap].isWrite = io_isWrite;
   state[newMap].writeData = io_writeData;
   state[newMap].cancel = io_cancel;
+  state[newMap].instrOp = (CacheOp)io_instrOp;
+  state[newMap].instrOk = io_instrOk;
 
   if (state[newMap].mainState != state[oldMap].mainState) {
     dclog("mainState Change: {} -> {}", mainStateStr[state[oldMap].mainState],
@@ -85,6 +108,20 @@ extern "C" void v_difftest_CacheRun(char io_mainState, svBit io_hasValid,
             ", valid: {:b}, dirty: {:b}, data: {}",
             (uint8_t)io_victimWay, (word_t)io_vicTag, io_vicValid, io_vicDirty,
             res.str());
+    }
+  }
+  if (io_mainState == instr) {
+    if (state[newMap].mainState != state[oldMap].mainState)
+      dclog("EXEC {:s}", cacheOpStr[(CacheOp)io_instrOp]);
+    else {
+      if (state[newMap].instrOp != state[oldMap].instrOp)
+        mycpu_log->error(fmt::format("CacheOp not same {:b} -> {:b}",
+                                     state[oldMap].instrOp,
+                                     state[newMap].instrOp));
+      if (state[newMap].instrOk != state[oldMap].instrOk)
+        dclog("FIN {:s}", cacheOpStr[(CacheOp)io_instrOp]);
+      if (io_instrRetire)
+        dclog("RETIRE {:s}", cacheOpStr[(CacheOp)io_instrOp]);
     }
   }
   if (state[newMap].writeState != state[oldMap].writeState)
