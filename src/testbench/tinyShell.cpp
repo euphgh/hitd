aeedea93ff1833ed74394ab6601c32aa9d4bafe0
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-07-26 21:54:45 +0800

    [tinyShell]: remove all fork

--- src/testbench/tinyShell.cpp
+++ src/testbench/tinyShell.cpp
@@ -1,6 +1,7 @@
 #include "common.hpp"
 #include "fmt/core.h"
 #include "nemu/isa.hpp"
+#include "paddr/paddr_interface.hpp"
 #include "soc.hpp"
 #include "testbench/sim_state.hpp"
 #include <cstdlib>
@@ -77,7 +78,7 @@ vector<tuple<string, function<int(vector<string> &)>, string>> built_in_cmds = {
 };
 
 // 执行命令
-int exec_cmd(vector<string> &cmd_tokens) {
+static void exec_cmd(vector<string> &cmd_tokens) {
   // 获取命令和参数
   char **args = new char *[cmd_tokens.size() + 1];
   for (int i = 0; i < cmd_tokens.size(); i++) {
@@ -88,29 +89,15 @@ int exec_cmd(vector<string> &cmd_tokens) {
   // 如果命令在内置命令字典中，则执行内置命令
   for (auto it : built_in_cmds) {
     if (get<0>(it) == cmd_tokens[0]) {
-      return get<1>(it)(cmd_tokens);
+      get<1>(it)(cmd_tokens);
+      return;
     }
   }
-
-  // 否则，调用外部命令
-  pid_t pid = fork();
-  if (pid == 0) {
-    execvp(args[0], args);
-    cerr << "execvp failed: " << strerror(errno) << endl;
-    exit(EXIT_FAILURE);
-  } else if (pid < 0) {
-    cerr << "fork failed: " << strerror(errno) << endl;
-    return -1;
-  } else {
-    int status;
-    waitpid(pid, &status, 0);
-    delete[] args;
-    return WEXITSTATUS(status);
-  }
+  print("not find command `{:s}`\n", cmd_tokens[0]);
 }
 
 // 解析输入的命令行
-void parse_cmd(const string &cmd_str) {
+static void parse_cmd(const string &cmd_str) {
   // 分割命令行
   vector<string> tokens;
   istringstream iss(cmd_str);
@@ -119,104 +106,8 @@ void parse_cmd(const string &cmd_str) {
     tokens.push_back(token);
   }
 
-  // 检查管道符号和重定向符号
-  auto pipe_it = find(tokens.begin(), tokens.end(), "|");
-  auto redirect_it =
-      find_if(tokens.begin(), tokens.end(), [](const string &token) {
-        return token == ">" || token == ">>";
-      });
-
-  // 如果存在管道符号
-  if (pipe_it != tokens.end()) {
-    // 获取第一个命令和第二个命令
-    vector<string> cmd1(tokens.begin(), pipe_it);
-    vector<string> cmd2(pipe_it + 1, tokens.end());
-
-    // 创建管道
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-      cerr << "pipe failed: " << strerror(errno) << endl;
-      return;
-    }
-
-    // 创建子进程并执行第一个命令
-    pid_t pid1 = fork();
-    if (pid1 == 0) {
-      close(pipefd[0]);
-      dup2(pipefd[1], STDOUT_FILENO);
-      close(pipefd[1]);
-      exit(exec_cmd(cmd1));
-    } else if (pid1 < 0) {
-      cerr << "fork failed: " << strerror(errno) << endl;
-      return;
-    }
-
-    // 创建子进程并执行第二个命令
-    pid_t pid2 = fork();
-    if (pid2 == 0) {
-      close(pipefd[1]);
-      dup2(pipefd[0], STDIN_FILENO);
-      close(pipefd[0]);
-      exit(exec_cmd(cmd2));
-    } else if (pid2 < 0) {
-      cerr << "fork failed: " << strerror(errno) << endl;
-      return;
-    }
-
-    // 等待子进程完成
-    close(pipefd[0]);
-    close(pipefd[1]);
-    int status2;
-    waitpid(pid1, &status2, 0);
-    waitpid(pid2, &status2, 0);
-    if (WIFEXITED(status2)) {
-      return;
-    } else {
-      exit(EXIT_FAILURE);
-    }
-
-    // 如果存在重定向符号
-  } else if (redirect_it != tokens.end()) {
-    // 获取命令和文件名
-    vector<string> cmd(tokens.begin(), redirect_it);
-    string filename(*(redirect_it + 1));
-
-    // 打开文件
-    int fd;
-    if (*(redirect_it) == ">") {
-      fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    } else {
-      fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0644);
-    }
-    if (fd == -1) {
-      cerr << "open failed: " << strerror(errno) << endl;
-      return;
-    }
-
-    // 将输出重定向到文件
-    pid_t pid = fork();
-    if (pid == 0) {
-      dup2(fd, STDOUT_FILENO);
-      close(fd);
-      exit(exec_cmd(cmd));
-    } else if (pid < 0) {
-      cerr << "fork failed: " << strerror(errno) << endl;
-      return;
-    } else {
-      int status;
-      waitpid(pid, &status, 0);
-      close(fd);
-      if (WIFEXITED(status)) {
-        return;
-      } else {
-        exit(EXIT_FAILURE);
-      }
-    }
-
-    // 否则执行单个命令
-  } else {
-    exec_cmd(tokens);
-  }
+  // 执行单个命令
+  exec_cmd(tokens);
 }
 
 void tinyShell() {
